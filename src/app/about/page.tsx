e9c5e183363bba63ca4da39190aa910c4c9fb79f
Yazin <yazin@amalinvest.com>
2023-06-25 11:13:32 +0300

    Working server component with local component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,14 @@
 import { kv } from '@vercel/kv';
+import ClientButton from '@/components/ClientButton'
 
 export default async function Page() {
-    const user = await kv.hgetall('user:me');
-    console.log(user)
+    const user: any = await kv.get('user:me');
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h1>About</h1>
+            <h1>About {user.name}</h1>
+
+            <ClientButton user={user} />
         </main>
     )
 }
