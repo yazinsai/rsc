6af33df91312bfd341ecf29af247e6b20035b63c
Yazin <yazin@amalinvest.com>
2023-06-25 10:26:14 +0300

    Fix build

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,10 +2,11 @@ import { kv } from '@vercel/kv';
 
 export default async function Page() {
     const user = await kv.hgetall('user:me');
+    console.log(user)
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h1>About {user}</h1>
+            <h1>About</h1>
         </main>
     )
 }
